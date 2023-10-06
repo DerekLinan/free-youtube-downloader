7bca2e2da55d535d04de36f97a3bd013fb63dc9a
DerekLinan <4261674+DerekLinan@users.noreply.github.com>
2023-10-06 04:53:56 -0600

    feat(popup): hide actions for invalid page
    - when youtube downloader cannot download a video from this page, hide the button actions and show the error-content

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,4 +1,5 @@
 import './popup.css';
+import { validateURL } from 'ytdl-core';
 import { QUALITIES } from '../utils/messager.js';
 
 async function GetActiveTab() {
@@ -19,21 +20,33 @@ async function Download(quality) {
   // TODO tell background to add the download at this url with specified quality to the queue
 }
 
+const { url: currentURL } = await GetActiveTab();
+
 /**
- * Initialize popup button listeners
+ * Check if ydl can download from this page
  */
-document
-  .getElementById('best')
-  .addEventListener('click', () => Download(QUALITIES.BEST));
-document
-  .getElementById('video')
-  .addEventListener('click', () => Download(QUALITIES.VIDEO));
-document
-  .getElementById('audio')
-  .addEventListener('click', () => Download(QUALITIES.AUDIO));
-document
-  .getElementById('fast')
-  .addEventListener('click', () => Download(QUALITIES.FAST));
-document
-  .getElementById('custom')
-  .addEventListener('click', () => Download(QUALITIES.CUSTOM));
+if (!validateURL(currentURL)) {
+  document.getElementById('button-group').classList.add('hidden');
+  document.getElementById('asterisk-text').classList.add('hidden');
+
+  document.getElementById('error-content').classList.remove('hidden');
+} else {
+  /**
+   * Initialize popup button listeners
+   */
+  document
+    .getElementById('best')
+    .addEventListener('click', () => Download(QUALITIES.BEST));
+  document
+    .getElementById('video')
+    .addEventListener('click', () => Download(QUALITIES.VIDEO));
+  document
+    .getElementById('audio')
+    .addEventListener('click', () => Download(QUALITIES.AUDIO));
+  document
+    .getElementById('fast')
+    .addEventListener('click', () => Download(QUALITIES.FAST));
+  document
+    .getElementById('custom')
+    .addEventListener('click', () => Download(QUALITIES.CUSTOM));
+}
